33d88a835e1f9f2876726ede66c912e3ef7a3c82
srod-douglas <srod.douglas@gmail.com>
2023-05-31 01:50:26 -0300

    feat: initial register

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -29,25 +29,27 @@ const Login = () => {
         <>
             <main id='login' className='container flex flex-col h-full max-h-screen max-w-full justify-center m-0 bg-gradient-to-t from-slate-950 to-slate-700'>
                     
-                <section className='flex flex-col h-4/5 max-h-screen max-w-full justify-center items-center'>
-                    <form noValidate onSubmit={handleSubmit(submit)} className=' w-3/12 container flex flex-col justify-around  h-full max-h-screen p-9 bg-slate-800 drop-shadow-2xl rounded-xl'>
+                <section className='flex flex-col h-2/3 max-h-screen max-w-full w-full justify-center items-center'>
+                    <form noValidate onSubmit={handleSubmit(submit)} className=' w-3/12 container flex flex-col justify-around  h-full max-h-screen p-6 bg-slate-800 drop-shadow-2xl rounded-xl'>
 
-                        <h2 className='text-center text-2xl text-gray-300 sm:text-3xl font-extrabold dark:text-slate-200 opacity-90'>Clients Manager</h2>
+                    <div className='flex justify-center gap-1 place-items-center text-center'>
+                            <h3 className='text-center text-gray-300 sm:text-xl font-normal dark:text-slate-200 opacity-90'>Clients Manager | </h3>
+
+                            <h4  className='mt-1 text-center text-gray-300 sm:text-base font-thin dark:text-slate-200 opacity-90 tracking-widest'> login</h4>
+                        </div>
 
                         <input placeholder='email' {...register('email')} className='h-12 pl-3 placeholder-gray-500 placeholder-opacity-100 rounded bg-slate-900 outline-none text-gray-300 ring-black caret-slate-500 ' />
-                        {errors.email && <span className='text-red-500 -mt-16 ml-3'>{errors.email.message}</span>}
-                        {/* {
-                            view && errors.password && errors.email ? <EyeOffIcon onClick={() => changeIcon()} className='h-7 w-7 absolute top-2/4 left-3/4 ml-8' /> : <EyeIcon onClick={() => changeIcon()} className='h-7 w-7 absolute top-2/4 left-3/4 ml-8' />
-                        } */}
+                        {errors.email && <span className='text-red-500 -mt-14 ml-3'>{errors.email.message}</span>}
 
                         <input placeholder='password' type={view ? 'text' : 'password'} {...register('password')} className='h-12 pl-3 placeholder-gray-500 placeholder-opacity-100 rounded bg-slate-900 outline-none text-gray-300 ring-black caret-slate-500' />
-                        {errors.password && <span className='text-red-500 -mt-16 ml-3'>{errors.password.message}</span>}
+                        {errors.password && <span className='text-red-500 -mt-14 ml-3'>{errors.password.message}</span>}
                         
                         {
                             view ? <EyeOffIcon onClick={() => changeIcon()} className='h-7 w-7 absolute top-2/4 left-3/4 ml-8' /> : <EyeIcon onClick={() => changeIcon()} className='h-7 w-7 absolute top-2/4 left-3/4 ml-8' />
                         }
 
                         <button type='submit' className='h-12 w-full text-gray-300 self-center bg-gradient-to-t from-slate-950 opacity-50 hover:opacity-100'>login</button>
+
                         <div className='flex flex-col h-1.5 self-center justify-center place-items-center gap-5'>
                             <span className='text-gray-300'>forgot password? <Link className='text-gray-300 hover:underline hover:text-white decoration-white ' to={'/forgot'}>redefine here</Link></span>
                             <span className='text-gray-300'>don't have an account? <Link to='/register' className='text-gray-300 hover:underline hover:text-white'>sign up</Link></span>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -23,34 +23,52 @@ function Register () {
 
     return (
         <>
-            <main>
-                <section>
-                    <form onSubmit={handleSubmit(submit)}>
-                        <h2>Clients Manager</h2>
-                        <h3>New Register</h3>
+            <main className='container flex flex-col h-full max-h-screen max-w-full justify-center m-0 bg-gradient-to-t from-slate-950 to-slate-700'>
 
-                        <input placeholder='first name' {...register("first_name")} />
-                        {errors.first_name && <span>{errors.first_name.message}</span>}
+                <section className='flex flex-col h-5/6 max-h-screen max-w-full justify-center items-center'>
 
-                        <input placeholder='last name' {...register("last_name")}/>
-                        {errors.last_name && <span>{errors.last_name.message}</span>}
+                    <form onSubmit={handleSubmit(submit)} className=' w-3/12 container flex flex-col justify-around  h-full max-h-screen p-6 bg-slate-800 drop-shadow-2xl rounded-xl'>
 
-                        <input placeholder='phone' {...register("phone")}/>
-                        {errors.phone && <span>{errors.phone.message}</span>}
+                        {/* <h2 className='text-center text-gray-300 sm:text-3xl font-extrabold dark:text-slate-200 opacity-90'>Clients Manager</h2> */}
+                        <div className='flex justify-center gap-1 place-items-center text-center'>
+                            <h3 className='text-center text-gray-300 sm:text-xl font-normal dark:text-slate-200 opacity-90'>Clients Manager | </h3>
 
-                        <input placeholder='email' {...register("email")}/>
-                        {errors.email && <span>{errors.email.message}</span>}
+                            <h4  className='text-center text-gray-300 sm:text-base font-thin dark:text-slate-200 opacity-90 tracking-widest'> new register</h4>
+                        </div>
 
-                        <input placeholder='password' {...register("password")} />
-                        {errors.password && <span>{errors.password.message}</span>}
+                        <input placeholder='first name' {...register("first_name")} className='h-12 pl-3 placeholder-gray-500 placeholder-opacity-100 rounded bg-slate-900 outline-none text-gray-300 ring-black caret-slate-500 '/>
 
-                        <input placeholder='confirm password' {...register("confirm_password")} />
-                        {errors.confirm_password && <span>{errors.confirm_password.message}</span>}
+                        {errors.first_name && <span className='text-red-500 ml-3 -mt-2'>{errors.first_name.message}</span>}
 
-                        <button>register</button>
-                        <span>or, already have an account?</span>
+                        <input placeholder='last name' {...register("last_name")} className='h-12 pl-3 placeholder-gray-500 placeholder-opacity-100 rounded bg-slate-900 outline-none text-gray-300 ring-black caret-slate-500 '/>
 
-                        <Link to={'/login'}>login here</Link>
+                        {errors.last_name && <span className='text-red-500 ml-3 -mt-2'>{errors.last_name.message}</span>}
+
+                        <input placeholder='phone' {...register("phone")} className='h-12 pl-3 placeholder-gray-500 placeholder-opacity-100 rounded bg-slate-900 outline-none text-gray-300 ring-black caret-slate-500 '/>
+
+                        {errors.phone && <span className='text-red-500 ml-3 -mt-2'>{errors.phone.message}</span>}
+
+                        <input placeholder='email' {...register("email")} className='h-12 pl-3 placeholder-gray-500 placeholder-opacity-100 rounded bg-slate-900 outline-none text-gray-300 ring-black caret-slate-500 '/>
+
+                        {errors.email && <span className='text-red-500 ml-3 -mt-2'>{errors.email.message}</span>}
+
+                        <input placeholder='password' {...register("password")} className='h-12 pl-3 placeholder-gray-500 placeholder-opacity-100 rounded bg-slate-900 outline-none text-gray-300 ring-black caret-slate-500 '/>
+
+                        {errors.password && <span className='text-red-500 ml-3 -mt-2'>{errors.password.message}</span>}
+
+                        <input placeholder='confirm password' {...register("confirm_password")} className='h-12 pl-3 placeholder-gray-500 placeholder-opacity-100 rounded bg-slate-900 outline-none text-gray-300 ring-black caret-slate-500 ' />
+
+                        {errors.confirm_password && <span className='text-red-500 ml-3 -mt-2'>{errors.confirm_password.message}</span>}
+
+                        <button className='h-12 w-full text-gray-300 self-center bg-gradient-to-t from-slate-950 opacity-50 hover:opacity-100'>register</button>
+
+                        <div className='flex flex-col self-center justify-center place-items-center gap-5'>
+
+                            <span className='text-gray-300'>or, already have an account?</span>
+                            <Link to={'/login'} className='text-gray-300 hover:underline hover:text-white'>login here</Link>
+
+                        </div>
+
 
                     </form>
                 </section>
