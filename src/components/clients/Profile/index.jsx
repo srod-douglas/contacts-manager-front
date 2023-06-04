35ab8a99de00b3fb77b1e23ec33e1096cfd3682c
srod-douglas <srod.douglas@gmail.com>
2023-06-04 03:52:36 -0300

    feat: details and adjusts all components and pages

--- src/components/clients/Profile/index.jsx
+++ src/components/clients/Profile/index.jsx
@@ -1,32 +1,19 @@
 import { useContext, useEffect, useState } from "react"
+
 import { ClientContext } from "../../../contexts/client"
-import { CheckCircleIcon, DeviceMobileIcon, FingerPrintIcon, MailIcon, UserCircleIcon, TrashIcon, ChevronDoubleDownIcon, ChevronDoubleUpIcon, ExclamationIcon, CakeIcon } from "@heroicons/react/outline"
-import { useForm } from "react-hook-form"
-import { yupResolver } from "@hookform/resolvers/yup"
-import { SchemaUpdateClient } from '../../Validators/SchemaUpdateClient' 
+import { UpdateProfile } from "./UpdateProfile"
+import { DeleteProfile } from "./DeleteProfile"
+import './styles.css'
+
+import { CheckCircleIcon, DeviceMobileIcon, FingerPrintIcon, MailIcon, UserCircleIcon, ChevronDoubleDownIcon, ChevronDoubleUpIcon, ExclamationIcon, CakeIcon } from "@heroicons/react/outline"
 
 export const Profile = () => {
-    const { client, clientInfos, updateClient } = useContext(ClientContext)
+    const { clientInfos } = useContext(ClientContext)
 
     const clientLocal = JSON.parse(localStorage.getItem('@client'))
+
     const [isDeleteMenu, setDeleteMenu] = useState(false)
     const [isUpdateMenu, setUpdateMenu] = useState(false)
-
-    const { register, handleSubmit, formState: { errors }, reset } = useForm({
-        resolver: yupResolver(SchemaUpdateClient)
-    })
-
-    const submit = async (data) => {
-        Object.keys(data).forEach((key) => {
-            if(data[key] === '') delete data[key]
-        })
-
-        await updateClient(clientLocal.id, data)
-        setUpdateMenu(false)
-
-        await clientInfos(clientLocal.id)
-        reset()
-    }
 
     const convertDate = () => {
 
@@ -41,7 +28,7 @@ export const Profile = () => {
 
     useEffect(() => {
         clientInfos(+clientLocal.id)
-        return () => console.log('desmontou o component profile')
+        return () => {}
     }, [])
 
     const handleDeleteMenu = () => {
@@ -64,180 +51,94 @@ export const Profile = () => {
 
     return (
         <section className='flex'>
-            <div
-              className="animate-[fade_.5s_ease-in] h-full w-2/4 p-4 flex bg-black bg-opacity-20 backdrop-blur-md rounded-3xl transition ease-in delay-0 "
-              key={clientLocal.id}
-            >
+            <div className="container-profile">
 
-                <div className='flex flex-col justify-around w-1/4'>
-                
-                    <div className="w-full h-full flex flex-col justify-around">
-                        <UserCircleIcon className="h-26 stroke-1 stroke-white" />
-                        <div className="flex gap-2 w-full">
+                <div className='container-left'>
+                    <div className="container-left-infos">
+
+                        <UserCircleIcon className="icon-client" />
+
+                        <div className="container-infos">
                             <div className="flex gap-2 w-2/5">
-                                <FingerPrintIcon className="w-4 stroke-white" />
-                                <h4 className="text-white text-xl">{clientLocal.id}</h4>
-                                <CheckCircleIcon className="w-4 stroke-green-400"/>
+                                <FingerPrintIcon className="icon-white" />
+                                <h4 className="texts">{clientLocal.id}</h4>
+                                <CheckCircleIcon className="icon-green"/>
                             </div>
                         </div>
 
-                        <div className="flex gap-2 w-full">
-                            <CheckCircleIcon className="w-4 stroke-white" />
-                            <h4 className="text-white text-xl">{clientLocal.first_name}</h4>
+                        <div className="container-infos">
+                            <CheckCircleIcon className="icon-white" />
+                            <h4 className="texts">{clientLocal.first_name}</h4>
                             {clientLocal.last_name &&
                                 (
                                     <>
-                                        <h4 className="text-white text-xl">{clientLocal.last_name}</h4>
-                                        <CheckCircleIcon className="w-4 stroke-green-400"/>
+                                        <h4 className="texts">{clientLocal.last_name}</h4>
+                                        <CheckCircleIcon className="icon-green"/>
                                     </>
                                 )
                             }
                         </div>
 
-                        <div className='flex gap-2 w-full'>
-                            <MailIcon className="w-4 stroke-white" />
-                            <h4 className="text-white text-xl">{clientLocal.email}</h4>
-                            <CheckCircleIcon className="w-4 stroke-green-400"/>
+                        <div className='container-infos'>
+                            <MailIcon className="icon-white" />
+                            <h4 className="texts">{clientLocal.email}</h4>
+                            <CheckCircleIcon className="icon-green"/>
                         </div>
                         
-                        <div className='flex gap-2 w-full'>
-                            <DeviceMobileIcon className="w-4 stroke-white" />
-                            <h4 className="text-white text-xl">{clientLocal.phone ? clientLocal.phone : 'not informed'}</h4>
+                        <div className='container-infos'>
+                            <DeviceMobileIcon className="icon-white" />
+                            <h4 className="texts">{clientLocal.phone ? clientLocal.phone : 'not informed'}</h4>
                             {!clientLocal.phone ? 
                                 (
-                                    <ExclamationIcon className="w-4 stroke-red-600" />
+                                    <ExclamationIcon className="icon-error" />
                                 ) : (
-                                    <CheckCircleIcon className="w-4 stroke-green-400"/>
+                                    <CheckCircleIcon className="icon-green"/>
                                 )
                             }
                         </div>
 
-                        <div className='flex gap-2 w-full'>
-                            <CakeIcon className="w-4 stroke-white" />
-                            <h4 className="text-white text-xl">{convertDate()}</h4>
-                            <CheckCircleIcon className="w-4 stroke-green-400"/>
+                        <div className='container-infos'>
+                            <CakeIcon className="icon-white" />
+                            <h4 className="texts">{convertDate()}</h4>
+                            <CheckCircleIcon className="icon-green"/>
                         </div>
                     </div>
                 </div>
 
-                <div className='flex flex-col items-center justify-start w-3/4'>
+                <div className='container-right'>
+                    <div className='container-right-infos'>
 
-                    <div className='w-full h-4 flex justify-start'>
-
-                        <div onClick={handleUpdateMenu} className='w-2/4 flex items-center justify-around'>
-                            <h2 className='text-white text-xl'>Update account</h2>
+                        <div onClick={handleUpdateMenu} className='container-right-titles'>
+                            <h2 className='texts'>Update account</h2>
                             {
                                 isUpdateMenu ?
                                     (
-                                        <ChevronDoubleUpIcon className="w-4 stroke-white"/>
+                                        <ChevronDoubleUpIcon className="icon-white"/>
                                     ) : (
-                                        <ChevronDoubleDownIcon className="w-4 stroke-white"/>
+                                        <ChevronDoubleDownIcon className="icon-white"/>
                                     )
                             }
                         </div>
 
-                        <div onClick={handleDeleteMenu} className='w-2/4 flex items-center justify-around'>
-
-                                <h2 className='text-white text-xl'>Delete account</h2>
-                                    {
-                                        isDeleteMenu ?
-                                            (
-                                                <ChevronDoubleUpIcon className="w-4 stroke-white"/>
-                                            ) : (
-                                                <ChevronDoubleDownIcon className="w-4 stroke-white"/>
-                                            )
-                                    }
-
-
+                        <div onClick={handleDeleteMenu} className='container-right-titles'>
+                            <h2 className='texts'>Delete account</h2>
+                                {
+                                    isDeleteMenu ?
+                                        (
+                                            <ChevronDoubleUpIcon className="icon-white"/>
+                                        ) : (
+                                            <ChevronDoubleDownIcon className="icon-white"/>
+                                        )
+                                }
                         </div>
                     </div>
-
                     {isDeleteMenu && (
-                        <div className='animate-[fade_.5s_ease-in] flex flex-col items-center justify-around h-full'>
-                            <h2 className='text-red-600'>are you sure?</h2>
-                            <p>
-                                if you delete your account all your contacts will be lost
-                            </p>
-                            <button className='w-full h-1/6 rounded-lg bg-red-600 text-white'>
-                                delete my account
-                            </button>
-                            <button className='w-full h-1/6 rounded-lg bg-black text-white' onClick={() => setDeleteMenu(!isDeleteMenu)}>cancel</button>
-                        </div>
-
+                        <DeleteProfile handleDeleteMenu={handleDeleteMenu} />
                     )}
+
                     {isUpdateMenu && (
-                        <form className='animate-[fade_.5s_ease-in] max-w-full w-full backdrop-blur-none shadow-none' onSubmit={() => handleSubmit(submit)}>
-
-                            <div className='form-div-titles w-full'>
-                                <h2 className='text-center text-gray-300 text-xl font-extralight '>Update your contact</h2>
-                            </div>
-
-                            <fieldset>
-                                <input className='input' placeholder={clientLocal.first_name} {...register("first_name")} />
-                                {errors.first_name && <span>{errors.last_name.message}</span>}
-                                {!clientLocal.first_name ?
-                                    (
-                                        <ExclamationIcon className="w-4 stroke-red-600 absolute top-4 right-4" />
-                                    ) : (
-                                        <CheckCircleIcon className="w-4 stroke-green-400 absolute top-4 right-4"/>
-                                    )
-                                }
-                            </fieldset>
-
-                            <fieldset>
-                                <input className='input' placeholder={clientLocal.last_name ? clientLocal.last_name : 'add your last name'} {...register("last_name")} />
-                                {errors.last_name && <span>{errors.last_name.message}</span>}
-                                {!clientLocal.last_name ? 
-                                    (
-                                        <ExclamationIcon className="w-4 stroke-red-600 absolute top-4 right-4" />
-                                    ) : (
-                                        <CheckCircleIcon className="w-4 stroke-green-400 absolute top-4 right-4"/>
-                                    )
-                                }
-                            </fieldset>
-
-                            <fieldset>
-                                <input className='input' placeholder={clientLocal.email} {...register("email")} />
-                                {errors.email && <span>{errors.email.message}</span>}
-                                {!clientLocal.email ? 
-                                    (
-                                        <ExclamationIcon className="w-4 stroke-red-600 absolute top-4 right-4" />
-                                    ) : (
-                                        <CheckCircleIcon className="w-4 stroke-green-400 absolute top-4 right-4"/>
-                                    )
-                                }
-                            </fieldset>
-
-                            <fieldset>
-                                <input className='input' type='number' placeholder={clientLocal.phone ? clientLocal.phone : 'add your phone'} {...register("phone")} />
-                                {errors.phone && <span>{errors.phone.message}</span>}
-                                {!clientLocal.phone || errors.phone ? 
-                                    (
-                                        <ExclamationIcon className="w-4 stroke-red-600 absolute top-4 right-4" />
-                                    ) : (
-                                        <CheckCircleIcon className="w-4 stroke-green-400 absolute top-4 right-4"/>
-                                    )
-                                }
-                            </fieldset>
-
-                            <div className='flex h-2/6 justify-around'>
-                                <button type='button' 
-                                    className='h-full w-1/3 bg-lime-950 hover:bg-lime-900 text-white rounded-lg' 
-                                    onClick={handleSubmit(submit)}>
-                                        update
-                                    </button>
-
-                                <button type='button' 
-                                    className='h-full w-1/3 bg-red-300 bg-opacity-50 hover:bg-red-500 rounded-lg text-white' 
-                                    onClick={() => setUpdateMenu(false)}>
-                                        cancel
-                                </button>
-                            </div>
-
-                        </form>
-
+                        <UpdateProfile setUpdateMenu={setUpdateMenu}/>
                     )}
-
                 </div>
             </div>
         </section>
--- src/components/contacts/DeleteContact/index.jsx
+++ src/components/contacts/DeleteContact/index.jsx
@@ -22,8 +22,8 @@ export const DeleteContact = () => {
 
     return (
         <>
-            <section>
-                <form className='animate-[fade_.5s_ease-in]' onSubmit={handleSubmit(submit)}>
+            <section className='flex justify-center'>
+                <form className='animate-[fade_.5s_ease-in] h-1/3' onSubmit={handleSubmit(submit)}>
                     <div className='form-div-titles'>
                         <h2 className='text-center text-gray-300 text-xl font-extralight dark:text-slate-200 opacity-90'>Delete a contact</h2>
                     </div>
@@ -31,7 +31,7 @@ export const DeleteContact = () => {
                         <input className='input' type='number' autoComplete='off' placeholder='ID contact' {...register("id")} />
                         {errors.id && <span className='input--error'>{errors.id.message}</span>}
                     </fieldset>
-                    <button id='btn-login' type='submit'>delete</button>
+                    <button className='w-full h-1/5 rounded-lg bg-transparent text-red-700 text-xl transition ease-in duration-200 hover:bg-red-700 hover:text-white' type='submit'>Delete</button>
                 </form>
             </section>
         </>
